286e9ce605257d9905f759b77297ad5eb0bb8887
DocenkoG <docn@mail.ru>
2019-02-19 19:40:50 +0300

    c:

--- at_trade.py
+++ at_trade.py
@@ -37,8 +37,7 @@ def main( ):
 
     if  at_trade_downloader.download( myname ) :
         at_trade_converter.convert2csv( myname )
-        shutil.copy2( myname + '.csv', 'c://AV_PROM/prices/' + myname +'/'+ myname + '.csv')
-        shutil.copy2( 'python.log', 'c://AV_PROM/prices/' + myname +'/'+ 'python.log')
+
 
 
 if __name__ == '__main__':
