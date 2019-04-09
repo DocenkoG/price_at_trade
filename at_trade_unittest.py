94ec6d0bb46aaa50b631344eea82ad97b86b92ca
DocenkoG <docn@mail.ru>
2019-04-09 19:05:57 +0300

    Закрываю старую технологию

--- at_trade_unittest.py
+++ at_trade_unittest.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import urllib
+import urllib.request
 import os, os.path
 
 filePrice = u'.\\tmp\\Price_Stock.zip'  
-urlPrice  = 'http://www.attrade.ru/files/TempFolder/Price_Stock.zip'
+urlPrice  = 'http://www.attrade.ru/pr30fe16ff-24c4-4bd1-be40-69cbd4593924/TempFolder/Price_Stock.zip'
 
-sss = urllib.urlopen(urlPrice).read()       #Скачиваем сначала страницу
+sss = urllib.request.urlopen(urlPrice).read()       #Скачиваем сначала страницу
+print(len(sss))
 
 if os.path.exists(filePrice):   os.remove(filePrice)
 f = open(filePrice, 'wb')                    #Теперь записываем файл
